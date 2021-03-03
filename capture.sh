a0d75735e2370f4e8677dc23d5c6266204e9b2c4
Braedenw400 <38366536+Braedenw400@users.noreply.github.com>
2021-03-03 15:32:50 -0500

    Update capture.sh

--- capture.sh
+++ capture.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 today=$( date +%m_%d_%H) 
-touch "acars_out_$today.txt"
+touch "acarsout_$today.txt"
 vdlm2dec -r 0 136.675 136.725 136.775 136.875 136.975 2>&1 | tee acars_out_$today.txt
